f53c0abc1b1b668645abfaaec4ed98ba7a7a1d16
Jonas Belouadi <jonas.belouadi@posteo.net>
2020-12-29 15:59:21 +0100

    Use absolute paths to find datasets.

--- main.py
+++ main.py
@@ -2,13 +2,15 @@
 from aligner import XMoverAligner
 from csv import reader
 from itertools import islice
-from os.path import isfile, join
+from os.path import isfile
 from gzip import open as gopen
 from urllib.request import urlretrieve
+from pathlib import Path
+
 
 filename = 'news-commentary-v15.de-en.tsv.gz'
 url = f'http://data.statmt.org/news-commentary/v15/training/{filename}'
-path = f'datasets/{filename}'
+path = (Path(__file__).parent / f'datasets/{filename}').resolve()
 samples = 3000
 
 if not isfile(path):
