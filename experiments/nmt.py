415f755cfdb699b85567ee3afac2fbe2ff6d81ad
Jonas Belouadi <jonas.belouadi@posteo.net>
2021-06-30 23:27:26 +0200

    Experiment with different max sentence length.

--- experiments/nmt.py
+++ experiments/nmt.py
@@ -9,13 +9,13 @@ import logging
 source_lang, target_lang = "de", "en"
 iterations = 1
 
-def nmt_tests(metric="cosine", weights=[0.8, 0.2]):
+def nmt_tests(metric="cosine", weights=[0.8, 0.2], max_len=30):
     aligner = XMoverNMTLMBertAlignScore(src_lang=source_lang, tgt_lang=target_lang, nmt_weights=weights,
             use_cosine=metric == "cosine")
-    dataset = DatasetLoader(source_lang, target_lang)
+    dataset = DatasetLoader(source_lang, target_lang, max_monolingual_sent_len=max_len)
     mono_src, mono_tgt = dataset.load("monolingual-align")
     eval_src, eval_system, eval_scores = dataset.load("scored")
-    suffix = f"{source_lang}-{target_lang}-awesome-{metric}-{aligner.mapping}-monolingual-align-{aligner.k}-{aligner.remap_size}-{len(mono_src)}"
+    suffix = f"{source_lang}-{target_lang}-awesome-{metric}-{aligner.mapping}-monolingual-align-{aligner.k}-{aligner.remap_size}-{len(mono_src)}-{max_len}"
     results, index = defaultdict(list), list(range(iterations + 1)) + [f"{iterations} + NMT"]
 
     logging.info("Evaluating performance before remapping.")
@@ -51,8 +51,10 @@ def nmt_tests(metric="cosine", weights=[0.8, 0.2]):
         results["rmse"].append(round(rmse, 2))
         results["mae"].append(round(mae, 2))
 
-    mono_src, mono_tgt = dataset.load("monolingual-train")
-    aligner.train(mono_src, mono_tgt, suffix=suffix+f"-{iterations}", overwrite=False, k=5 if metric=="cosine" else 1)
+    if not {'train_src', 'train_tgt'}.issubset(globals()):
+        global train_src, train_tgt
+        train_src, train_tgt = dataset.load("monolingual-train")
+    aligner.train(train_src, train_tgt, suffix=suffix+f"-{iterations}", overwrite=False, k=5 if metric=="cosine" else 1)
 
     logging.info("Evaluating performance with NMT model.")
     pearson, spearman = aligner.correlation(eval_src, eval_system, eval_scores)
@@ -80,6 +82,11 @@ def nmt_tests(metric="cosine", weights=[0.8, 0.2]):
 logging.basicConfig(level=logging.INFO, datefmt="%m-%d %H:%M", format="%(asctime)s %(levelname)-8s %(message)s")
 
 for weight in linspace(1, 0, 11):
-    logging.info(f"Using weight {weight} for cross-lingual XMoverScore and weight {1 - weight} for NMT system.")
+    print(f"Using weight {weight} for cross-lingual XMoverScore and weight {1 - weight} for NMT system.")
     print(*nmt_tests(metric="cosine", weights=[weight, 1 - weight]), sep="\n")
     print(*nmt_tests(metric="wmd", weights=[weight, 1 - weight]), sep="\n")
+
+for weight in linspace(1, 0, 11):
+    print(f"Using weight {weight} for cross-lingual XMoverScore and weight {1 - weight} for NMT system.")
+    print(*nmt_tests(metric="cosine", weights=[weight, 1 - weight], max_len=50), sep="\n")
+    print(*nmt_tests(metric="wmd", weights=[weight, 1 - weight], max_len=50), sep="\n")
